b0b747dcd0814dd16d3fabd0b62a669c185a9f5b
Moujan <moujan.n.namaghi@gmail.com>
2026-07-17 01:23:37 +0330

    Configure Entity Framework Core and database migration

---
+++ SimpleShop/Data/ApplicationDbContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SimpleShop.Models.Entities;
+
+namespace SimpleShop.Data;
+
+public class ApplicationDbContext : DbContext
+{
+    public ApplicationDbContext(
+        DbContextOptions<ApplicationDbContext> options)
+        : base(options) { }
+
+
+    public DbSet<Category> Categories { get; set; }
+
+    public DbSet<Product> Products { get; set; }
+
+    public DbSet<Order> Orders { get; set; }
+
+    public DbSet<OrderItem> OrderItems { get; set; }
+}
