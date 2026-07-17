ca6bddd06f5616b30eda5babc0b13c6f608570e7
Moujan <moujan.n.namaghi@gmail.com>
2026-07-17 14:20:07 +0330

    Implement shopping cart functionality

--- SimpleShop/Controllers/CartController.cs
+++ SimpleShop/Controllers/CartController.cs
@@ -1,12 +1,63 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using SimpleShop.Data;
+using SimpleShop.Services;
 
-namespace SimpleShop.Controllers
+namespace SimpleShop.Controllers;
+
+public class CartController : Controller
 {
-    public class CartController : Controller
+    private readonly ApplicationDbContext _context;
+    private readonly CartService _cartService;
+
+
+    public CartController(
+        ApplicationDbContext context,
+        CartService cartService)
     {
-        public IActionResult Index()
+        _context = context;
+        _cartService = cartService;
+    }
+
+
+
+    public IActionResult Index()
+    {
+        var cart = _cartService.GetCart();
+
+        return View(cart);
+    }
+
+
+
+    public async Task<IActionResult> Add(int id)
+    {
+        var product = await _context.Products
+            .FirstOrDefaultAsync(x => x.Id == id);
+
+
+        if (product == null)
         {
-            return View();
+            return NotFound();
         }
+
+
+        _cartService.AddToCart(product);
+
+
+        return RedirectToAction(
+            "Index",
+            "Cart");
+    }
+
+
+
+    public IActionResult Remove(int id)
+    {
+        _cartService.RemoveFromCart(id);
+
+
+        return RedirectToAction(
+            "Index");
     }
 }
