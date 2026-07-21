416d1a0822850f3dd75aef90d2c9f4464dd8734e
Moujan <moujan.n.namaghi@gmail.com>
2026-07-21 11:07:43 +0330

    fix navigation links and routing between admin and public areas

---
+++ SimpleShop/Areas/Admin/Controllers/UsersController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using SimpleShop.Models.Entities;
+
+namespace SimpleShop.Areas.Admin.Controllers;
+
+[Area("Admin")]
+[Authorize(Roles = "Admin")]
+public class UsersController : Controller
+{
+    private readonly UserManager<ApplicationUser> _userManager;
+
+    public UsersController(
+        UserManager<ApplicationUser> userManager)
+    {
+        _userManager = userManager;
+    }
+
+    public IActionResult Index()
+    {
+        var users = _userManager.Users
+            .ToList();
+
+        return View(users);
+    }
+}
